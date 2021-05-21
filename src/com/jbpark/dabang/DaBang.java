21575226e1d8cbbbab09578e39164e2b50b1d5be
kang min su <mickye_26@naver.com>
2021-05-21 17:31:31 +0900

    JBDaBang openning commit

---
+++ src/com/jbpark/dabang/DaBang.java
@@ -0,0 +1,15 @@
+package com.jbpark.dabang;
+
+public class DaBang {
+
+		enum TeaType{ 율무, 감잎차, 국화꽃, 보성녹차 }
+		public class CafeMain{
+
+			public static void main(String[] args) {
+			 System.out.println("환영합니다");
+			 System.out.println(TeaType.율무);
+			}
+
+	}
+
+}
---
+++ src/com/jbpark/dabang/TeaType.java
@@ -0,0 +1,10 @@
+package com.jbpark.dabang;
+
+public class TeaType {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
